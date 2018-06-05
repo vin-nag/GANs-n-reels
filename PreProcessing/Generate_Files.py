779ab3544da1276c300abb3fc2ef47b6e5565336
Zachary Northcott <39657296+ZacharyMN709@users.noreply.github.com>
2018-06-04 23:34:56 -0230

    Update Generate_Files.py

--- PreProcessing/Generate_Files.py
+++ PreProcessing/Generate_Files.py
@@ -1,5 +1,30 @@
 from PreProcessing import Cleaner as Clean
 from datetime import datetime
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+import ast
+
+
+def update_tunes():
+    url = 'https://raw.githubusercontent.com/adactio/TheSession-data/master/json/tunes.json'
+    try:
+        with closing(get(url, stream=True)) as resp:
+            with open('../Tunes/Tunes_raw.py', 'w') as f:
+                text = resp.content.decode('utf-8')
+                temp = ast.literal_eval(text)
+
+                f.write("created = '" + str(datetime.now()) + "'\n\n")
+                f.write("tunes = [\n")
+                while True:
+                    tune = temp.pop()
+                    f.write(str(tune))
+                    if len(temp) == 0: break
+                    f.write(",\n")
+                f.write("\n]")
+    except RequestException as e:
+        print(e)
+        return None
 
 
 def create_dict(tunes, types=None, meters=None, modes=None, check_abc=True):
@@ -39,12 +64,12 @@ def dict_to_file(cleaned, fname):
         f.write("\n}")
 
 
-def master_clean(py_out, stats_out=None, types=None, meters=None, modes=None, check_abc=True, default_folder='Tunes/'):
-    from Tunes import Tunes_raw
+def master_clean(py_out, stats_out=None, types=None, meters=None, modes=None, check_abc=True, default_folder='../Tunes/'):
+    from Tunes import Tunes_raw as raw
     from PreProcessing import Generate_Stats
-    tunes = Tunes_raw.tunes
+    tunes = sorted(raw.tunes, key=lambda x: int(x['setting']), reverse=True)
     clean = create_dict(tunes, types=types, meters=meters, modes=modes, check_abc=check_abc)
-    lst = [clean[d] for d in clean]
+    lst = sorted([clean[d] for d in clean], key=lambda x: int(x['setting']), reverse=True)
     Generate_Stats.parse_stats(lst, default_folder + stats_out)
     dict_to_file(lst, default_folder + py_out)
 
@@ -62,4 +87,5 @@ def common_time_clean():
 
 
 if __name__ == '__main__':
+    update_tunes()
     common_time_clean()
