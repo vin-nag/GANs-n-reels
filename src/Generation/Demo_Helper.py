c3ebf7ac040819436301dd7eada22dc250505406
Zachary Northcott <zmnorthcott@mun.ca>
2018-07-19 10:54:50 -0600

    Added definitions to the decoder, and finshed a helper function for the display notebook.

---
+++ src/Generation/Demo_Helper.py
@@ -0,0 +1,9 @@
+from src.Generation.Vectorizing import Decoding
+
+
+def demo_helper(vec):
+    decoder = Decoding.Decoder()
+    Decoding.pitches_to_img(vec, out='Display.png')
+    abc_dic = Decoding.decode_single_vector(vec)
+    decoder.set_time('1/16')
+    decoder.play_from_dict(abc_dic[0])
