d00a53d444425e2454759e479160c241747af1b6
Zachary Northcott <zmnorthcott@mun.ca>
2018-10-11 19:40:48 -0230

    Incorperatedh5 support for the decoder, allowing the Generator to continuously create new tunes, without having to transfer files like before.

--- src/Generation/Decoding/Decoding.py
+++ src/Generation/Decoding/Decoding.py
@@ -8,8 +8,14 @@ from src.Generation.Vectorizing import Vectorizer as Vec
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import keras
 
 VECTOR_DIR = '../../../Data/Vectors/'
+
+MAX_PITCH = 80
+MIN_PITCH = 53
+MID_PITCH = (MAX_PITCH + MIN_PITCH) / 2
+RANGE = MAX_PITCH - MIN_PITCH
 
 tritone = {0: 'z[=G2=G,2][=G2=G,2][=G2=G,2][_E8_E,8] z[=F2=F,2][=F2=F,2][=F2=F,2][=D8=D,]'}
 
@@ -61,25 +67,82 @@ def convert_note_list(lst):
     return out
 
 
+def decode_single_vector(array, presentation):
+    """
+    Takes an array of tunes in vectorized form and returns a dictionary of abc strings.
+    :param array: A 3 dimensional array of pieces, in the form pieces[tune][bar]
+    :return: A dictionary of abc strings.
+    """
+
+    def decode_song(vec):
+        dur = [vec[0][0]]
+        master = []
+        for x in vec:
+            for y in x:
+                if y == dur[0]:
+                    dur.append(y)
+                else:
+                    master.append(dur)
+                    dur = [y]
+        return master
+
+    if presentation:
+        print("Loading Vector Results from GAN...\n")
+        print("Selected Vector Generated")
+        print(array[0])
+        print("\nImage of the Vector")
+        plt.matshow(array[0].T, cmap=plt.get_cmap('Greys'), fignum=1)
+        plt.show()
+
+    tunes = dict()
+    c = 0
+    for x in array:
+        tune = decode_song(x)
+        tunes[c] = convert_note_list(tune)
+        c += 1
+
+    if presentation:
+        print("Decoding Vectors to ABC format...")
+        print(" ")
+        print("ABC Notation Generated")
+        print(tunes[0])
+        print(" ")
+
+    return tunes
+
+
+def parse_raw_abc(abc):
+    import re
+
+    abc = abc.replace('|', '')
+    vec = re.findall('([_=^]*[a-gzA-G][,\']*[\d]*/?[\d]*)', abc)
+    prev = vec[0]
+    tune = []
+    c = 0
+    for x in vec:
+        if x == prev:
+            c += 1
+        else:
+            tune.append(prev + str(c)) if c != 1 else tune.append(prev)
+            prev = x
+            c = 1
+    tune = ''.join(tune)
+    print(tune)
+    return tune
+
+
 class Decoder:
     """
     A class which helps control the variables for playing the music.
     """
-    def __init__(self, pitches, timing, tunes, time='1/48', key='Cmaj'):
+    def __init__(self, pitches, timing, tunes, time='1/48', key='Cmaj', gan=None):
         self.time = time
         self.key = key
         self.pitches = pitches
         self.timing = timing
         self.tunes = tunes
-
-    def gen_header(self):
-        header = '''X: 1\n
-                    T: AI Music\n
-                    R: reel\n
-                    M: 4/4\n
-                    L: ''' + self.time + '''\n
-                    K: ''' + self.key + '''\n'''
-        return header
+        self.gan = gan
+        self.header = '''X: 1\nT: AI Music\nR: reel\nM: 4/4\nL: {}\nK: {}\n'''.format(self.time, self.key)
 
     def set_key(self, key):
         self.key = key
@@ -88,22 +151,25 @@ class Decoder:
         self.time = time
 
     def save_tune(self, num, out='mid'):
-        abc = self.gen_header() + self.tunes[num]
+        abc = self.header + self.tunes[num]
         tune = converter.parse(abc, format='abc')
         for p in tune.parts: p.insert(0, instrument.Flute())
         switch = Audio_Converter.Converter(tune, out_type=out, num=num)
         switch.save_song()
 
-    def play(self, num=None):
+    def play(self, num=None, save=False, autosave=False):
         """
         This function takes in a song in ABC format string and plays it using music21 and pygame modules
         Note: Timidity must also be installed, as it's config file is required for the stream object.
         :param num: index of the ABC song
+        :param save: If true, prompts the user if they want to save the tune after playing.
+        superseded by autosave
+        :param autosave: If true, automatically save the song after playing it.
         :return: None
         """
 
         if num is None: num = random.randint(0, len(self.tunes)-1)
-        abc = self.gen_header() + self.tunes[num]
+        abc = self.header + self.tunes[num]
 
         try:
             # convert abc format to stream object and add the harp instrument
@@ -111,28 +177,29 @@ class Decoder:
             for p in tune.parts: p.insert(0, instrument.Flute())
 
             # Emit a tone to break up consecutive plays
-            player = StreamPlayer(converter.parse(self.gen_header() + 'z12C12z12', format='abc'))
+            player = StreamPlayer(converter.parse(self.header + 'z12C12z12', format='abc'))
             player.play()
 
             # play music using pygame stream player
             player = StreamPlayer(tune)
             print("Playing tune #{}\nABC:   {}\n".format(num, self.tunes[num]))
             player.play()
+
+            if autosave:
+                self.save_tune(num)
+            elif save and input('Would you like to save the song as a MIDI file? Y/N:   ').upper() == 'Y':
+                self.save_tune(num)
         except AccidentalException:
             print('Accidental Exception thrown in ABC:')
             print(abc)
 
-    def play_all(self):
-        for x in range(len(self.tunes)): self.play(x)
+    def play_all(self, save=False, autosave=False):
+        for x in range(len(self.tunes)): self.play(x, save, autosave)
 
-    def test_play(self, num=None, save=False):
-        if num is None: num = random.randint(0, len(self.tunes)-1)
-        self.play(num)
-        if not save: save = input('Would you like to save the song as a MIDI file? Y/N:   ').upper() == 'Y'
-        if save: self.save_tune(num)
-
-    def test_play_all(self):
-        for x in range(len(self.tunes)): self.test_play(x)
+    def play_infinite(self, save=False, autosave=False):
+        while True:
+            self.play_all(save, autosave)
+            if self.gan: self.refresh_tunes()
 
     def songs_to_greyscale(self, numcols=3, out='bars_bw.png'):
         """
@@ -167,15 +234,23 @@ class Decoder:
             plt.matshow(img.reshape(4, width * 4), cmap='gray', interpolation='nearest', fignum=0, aspect="auto")
         plt.savefig('../../../Data/Images/' + out)
 
-    def songs_to_colour(self, numcols=3, out='bars_cl.png'):
-        """
-        Takes an array of vectorized tunes and return an image representation of them.
-        :param numcols: Number of columns; Default -> 3
-        :param out: Name of output file; Default -> 'bars_cl.png'
-        :return: None. Saves an image to disk.
-        """
-        print("This is a stub!")
-        return
+    def refresh_tunes(self, presentation=False):
+        if self.gan:
+            noise = np.random.normal(0, 1, [1, 100])  # 20 arrays of noise of shape [100,]
+            generated_samples = self.gan.predict(noise)
+            generated_samples = generated_samples[:, :-1, 4:-4]
+            generated_samples = np.squeeze(generated_samples, axis=3)  # Remove colour channel
+            generated_samples = generated_samples.reshape([-1, 16, 16])
+
+            # Map from [-1, 1] to [MIN_PITCH, MAX_PITCH]
+            generated_samples = (RANGE * generated_samples) + MID_PITCH
+
+            # Make values discrete
+            generated_samples = generated_samples.astype(np.int32)
+            self.pitches = decode_single_vector(generated_samples, False)
+            self.tunes = decode_single_vector(self.pitches, presentation)
+        else:
+            print("Failed to update songs.")
 
     @classmethod
     def from_raw_abc(cls, abc, time='1/8', key='Cmaj', presentation=False, override=False):
@@ -201,53 +276,16 @@ class Decoder:
         return cls([], [], tunes, time, key)
 
     @classmethod
+    def from_h5(cls, generator="../src/Model/Trained/generator.h5", time='1/48', key='Cmaj', presentation=False):
+        gan = keras.models.load_model(generator)
+        obj = cls([], [], [], time, key, gan=gan)
+        obj.refresh_tunes(presentation)
+        return obj
+
+    @classmethod
     def from_single_vector(cls, pitches, time='1/48', key='Cmaj', presentation=False):
-
-        def decode_single_vector(array):
-            """
-            Takes an array of tunes in vectorized form and returns a dictionary of abc strings.
-            :param array: A 3 dimensional array of pieces, in the form pieces[tune][bar]
-            :return: A dictionary of abc strings.
-            """
-
-            def decode_song(vec):
-                dur = [vec[0][0]]
-                master = []
-                for x in vec:
-                    for y in x:
-                        if y == dur[0]:
-                            dur.append(y)
-                        else:
-                            master.append(dur)
-                            dur = [y]
-                return master
-
-            if presentation:
-                print("Loading Vector Results from GAN...\n")
-                print("Selected Vector Generated")
-                print(array[0])
-                print("\nImage of the Vector")
-                plt.matshow(array[0].T, cmap=plt.get_cmap('Greys'), fignum=1)
-                plt.show()
-
-            tunes = dict()
-            c = 0
-            for x in array:
-                tune = decode_song(x)
-                tunes[c] = convert_note_list(tune)
-                c += 1
-
-            if presentation:
-                print("Decoding Vectors to ABC format...")
-                print(" ")
-                print("ABC Notation Generated")
-                print(tunes[0])
-                print(" ")
-
-            return tunes
-
         if type(pitches) == str: pitches = load_vector(pitches)
-        tunes = decode_single_vector(pitches)
+        tunes = decode_single_vector(pitches, presentation)
 
         if presentation:
             print("Sample Image of Vector:")
@@ -257,56 +295,6 @@ class Decoder:
             print(tunes[0] + '\n')
 
         return cls(pitches, [], tunes, time, key)
-
-    @classmethod
-    def from_double_vector(cls, pitches, timing, time='1/48', key='Cmaj', presentation=False):
-
-        def decode_dual_vector(pitches, timing):
-            # TODO - Finish
-
-            def decode_song(pitches, timing):
-                x = 0
-                master = []
-                lst = list()
-                while x < len(pitches):
-                    lst.append(pitches[x])
-                    x += 1
-                    if x == len(pitches) or timing[x] == 1:
-                        master.append(lst)
-                        lst = []
-
-                return master
-
-            songs = dict()
-            for c in range(pitches.shape[0]):
-                tune = decode_song(pitches[c], timing[c])
-                songs[c] = convert_note_list(tune)
-            return songs
-
-        if type(pitches) == str: pitches = load_vector(pitches)
-        if type(timing) == str: timing = load_vector(timing)
-
-        return cls(pitches, timing, decode_dual_vector(pitches, timing), time, key)
-
-
-def parse_raw_abc(abc):
-    import re
-
-    abc = abc.replace('|', '')
-    vec = re.findall('([_=^]*[a-gzA-G][,\']*[\d]*/?[\d]*)', abc)
-    prev = vec[0]
-    tune = []
-    c = 0
-    for x in vec:
-        if x == prev:
-            c += 1
-        else:
-            tune.append(prev + str(c)) if c != 1 else tune.append(prev)
-            prev = x
-            c = 1
-    tune = ''.join(tune)
-    print(tune)
-    return tune
 
 
 if __name__ == '__main__':
@@ -326,8 +314,10 @@ if __name__ == '__main__':
     # decode = Decoder.from_raw_abc(parse_raw_abc(rand_tune), override=True, time='1/4')
     # decode.play()
 
-    decode = Decoder.from_single_vector('Major_Tunes_Generated_Oct11.npy', time='1/48')
-    decode.play(0)
+    # decode = Decoder.from_single_vector('Major_Tunes_Generated_Oct11.npy', time='1/48')
+    # decode.play(0)
     # decode.play_all()
     # decode.test_play_all()
     # decode.save_tune(1, out='mid')
+    decode = Decoder.from_h5()
+    decode.play_infinite()
